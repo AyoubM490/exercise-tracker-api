cac4bbccab373659b65ad125e662b6c2d150ebfe
AyoubMS <chahayb563@gmail.com>
2022-09-20 21:01:41 +0100

    remove bodyParser

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 
 require("dotenv").config();
@@ -8,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
