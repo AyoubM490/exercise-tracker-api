11dfde88f2e8a7a9a6d63bc5396e07aa410cb8c7
AyoubMS <chahayb563@gmail.com>
2022-09-22 10:26:21 +0100

    add server test

---
+++ server.test.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const request = require("supertest");
+
+require("dotenv").config();
+
+function createApp() {
+  const app = express();
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
+  return app;
+}
+
+describe("GET /", () => {
+  it("should return 200 OK", () => {
+    process.env.SERVER = true;
+    const app = createApp();
+    request(app).get("/").expect(200);
+  });
+});
